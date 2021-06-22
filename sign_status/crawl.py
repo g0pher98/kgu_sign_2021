0ce0880cd4528a4ee01a47c6c869a635d68ccb29
g0pher98 <mail@jseung.me>
2021-06-22 01:40:51 +0000

    fixes g0pher98/kgu_sign_2021#1

--- sign_status/crawl.py
+++ sign_status/crawl.py
@@ -4,16 +4,28 @@ import requests
 import re
 import json
 import datetime
+from bs4 import BeautifulSoup
 
 def get_status():
+    reply_list = []
     url = "http://www.kyonggi.ac.kr/boardView.kgu?bcode=B0077&id=331530&pid=34"
     res = requests.get(url)
-    raw = res.text
-
-    pattern = r"참여합니다"
-    find_res = re.findall(pattern, raw)
     
-    return len(find_res)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    all_reply_list = soup.select('.replyList form')
+    
+    for r in all_reply_list:
+        raw = r.get_text()
+        if "참여합니다" in raw:
+            check_data = raw   
+            if "-" in raw:  
+                check_data = raw.split('-')[1]                                          
+            if check_data not in reply_list:                                              
+                reply_list.append(check_data)
+    
+    return len(reply_list)
+
+
 
 def get_time():
     now = datetime.datetime.now()
